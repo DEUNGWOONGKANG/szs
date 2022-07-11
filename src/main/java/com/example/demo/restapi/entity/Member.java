0a541b62cf437fcac85918c1c11c7f4df5376ea2
강등웅 <deungwoong89@naver.com>
2022-07-11 17:38:12 +0900

    szs 프로젝트 신규생성

---
+++ src/main/java/com/example/demo/restapi/entity/Member.java
@@ -0,0 +1,25 @@
+package com.example.demo.restapi.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "member")
+public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long member_id;
+
+}
