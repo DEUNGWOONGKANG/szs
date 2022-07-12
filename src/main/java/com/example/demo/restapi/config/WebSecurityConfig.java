8f38d1c4532d31190d3f7b5bdf50f9e65f916f36
kang <kang@DESKTOP-HR1U185>
2022-07-13 00:14:14 +0900

    0712작업

---
+++ src/main/java/com/example/demo/restapi/config/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo.restapi.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable();
+		http.httpBasic().disable()
+			.authorizeHttpRequests()
+			.and();
+			
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("*"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+	}
+
+}
--- src/main/java/com/example/demo/restapi/entity/Member.java
+++ src/main/java/com/example/demo/restapi/entity/Member.java
@@ -1,25 +1,39 @@
 package com.example.demo.restapi.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Builder
-@Entity
+@Table(name="member")
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 @AllArgsConstructor
-@Table(name = "member")
+@Builder
 public class Member {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long member_id;
-
+	@Column(name = "userid", nullable = false, unique = true)
+	private String userId;
+	
+	@Column(name = "userpw", nullable = false)
+	private String password;
+	
+	@Column(name = "username", nullable = false)
+	private String name;
+	
+	@Column(name = "regno", nullable = false)
+	private String regNo;
+	
+	public void encodePassword(PasswordEncoder passwordEncoder){
+        this.password = passwordEncoder.encode(password);
+    }
 }
