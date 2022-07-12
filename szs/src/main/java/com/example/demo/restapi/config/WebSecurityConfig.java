98359efa69052536b51d5614cfc9f31a773fe9c1
강등웅 <deungwoong89@naver.com>
2022-07-12 18:15:18 +0900

    2022-07-12

--- szs/src/main/java/com/example/demo/restapi/config/WebSecurityConfig.java
+++ szs/src/main/java/com/example/demo/restapi/config/WebSecurityConfig.java
@@ -16,6 +16,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable();
+		http.httpBasic().disable()
+			.authorizeHttpRequests()
+			.and();
+			
 	}
 	
 	@Bean
